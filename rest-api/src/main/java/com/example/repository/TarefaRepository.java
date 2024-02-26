37c17b21f30bfed2a5ac721bf4650987699dc3b3
AtilaPrimo <atilaprimo@gmail.com>
2024-02-26 14:09:42 -0300

    Delete rest-api directory

--- rest-api/src/main/java/com/example/repository/TarefaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.model.Tarefa;
-
- public interface TarefaRepository extends JpaRepository<Tarefa, Integer>{
-
-}
